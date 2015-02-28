77a43f36dfadcaac45a6479fc6f0bf2abd4a9cd3
James Lee <JL@wirelessprvnat-172-17-177-120.near.illinois.edu>
2015-02-28 15:29:02 -0600

    Fixed ObjC

--- Pursuit/LoginControllerView.h
+++ Pursuit/LoginControllerView.h
@@ -9,9 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface LogControllerView : UIViewController <UITextFieldDelegate>
+{
+    BOOL checkboxSelected;
+    IBOutlet UIButton *checkboxButton;
+}
 @property (weak, nonatomic) IBOutlet UITextField *txtUsername;
 @property (weak, nonatomic) IBOutlet UITextField *txtPassword;
 - (IBAction)backgroudtap:(id)sender;
 -(IBAction)signinClicked:(id)sender;
+- (IBAction)checkboxButton:(id)sender;
+
 @end
 
--- Pursuit/LoginControllerView.m
+++ Pursuit/LoginControllerView.m
@@ -14,8 +14,20 @@
 @implementation LogControllerView
 
 - (void)viewDidLoad {
+    checkboxSelected = 0;
+
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)checkboxButton:(id)sender{
+    if (checkboxSelected == 0){
+        [checkboxButton setSelected:YES];
+        checkboxSelected = 1;
+    } else {
+        [checkboxButton setSelected:NO];
+        checkboxSelected = 0;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,75 +40,9 @@
 
 -(IBAction)signinClicked:(id)sender
 {
-    NSInteger success = 0;
-    @try {
-        
-        if([[self.txtUsername text] isEqualToString:@""] || [[self.txtPassword text] isEqualToString:@""] ) {
-            
-            [self alertStatus:@"Please enter Email and Password" :@"Sign in Failed!" :0];
-            
-        } else {
-            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[self.txtUsername text],[self.txtPassword text]];
-            NSLog(@"PostData: %@",post);
-            
-            NSURL *url=[NSURL URLWithString:@"https://dipinkrishna.com/jsonlogin.php"];
-            
-            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-            
-            NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-            
-            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-            [request setURL:url];
-            [request setHTTPMethod:@"POST"];
-            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-            [request setHTTPBody:postData];
-            
-            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
-            
-            NSError *error = [[NSError alloc] init];
-            NSHTTPURLResponse *response = nil;
-            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            
-            NSLog(@"Response code: %ld", (long)[response statusCode]);
-            
-            if ([response statusCode] >= 200 && [response statusCode] < 300)
-            {
-                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
-                NSLog(@"Response ==> %@", responseData);
-                
-                NSError *error = nil;
-                NSDictionary *jsonData = [NSJSONSerialization
-                                          JSONObjectWithData:urlData
-                                          options:NSJSONReadingMutableContainers
-                                          error:&error];
-                
-                success = [jsonData[@"success"] integerValue];
-                NSLog(@"Success: %ld",(long)success);
-                
-                if(success == 1)
-                {
-                    NSLog(@"Login SUCCESS");
-                } else {
-                    
-                    NSString *error_msg = (NSString *) jsonData[@"error_message"];
-                    [self alertStatus:error_msg :@"Sign in Failed!" :0];
-                }
-                
-            } else {
-                //if (error) NSLog(@"Error: %@", error);
-                [self alertStatus:@"Connection Failed" :@"Sign in Failed!" :0];
-            }
-        }
-    }
-    @catch (NSException * e) {
-        NSLog(@"Exception: %@", e);
-        [self alertStatus:@"Sign in Failed." :@"Error!" :0];
-    }
-    if (success) {
-        [self performSegueWithIdentifier:@"login_success" sender:self];
-    }
+
+    
+    
 }
 - (void) alertStatus:(NSString *)msg :(NSString *)title :(int) tag
 {
