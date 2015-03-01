03c32ad4ac68434e9f461bf4a9b08406fc70378c
James <JL@wirelessprvnat-172-17-177-120.near.illinois.edu>
2015-02-28 20:04:42 -0600

    update profile

---
+++ Pursuit/ProfileControllerView.h
@@ -0,0 +1,19 @@
+//
+//  ProfileControllerView.h
+//  Pursuit
+//
+//  Created by James Lee on 2/28/15.
+//  Copyright (c) 2015 TeamGeorgie. All rights reserved.
+//
+
+#ifndef Pursuit_ProfileControllerView_h
+#define Pursuit_ProfileControllerView_h
+#import <UIKit/UIKit.h>
+
+@interface ProfileControllerView : UITableViewController <UITextFieldDelegate,UIActionSheetDelegate>
+{
+}
+@end
+
+
+#endif
---
+++ Pursuit/ProfileControllerView.m
@@ -0,0 +1,23 @@
+//
+//  ProfileControllerView.m
+//  Pursuit
+//
+//  Created by James Lee on 2/28/15.
+//  Copyright (c) 2015 TeamGeorgie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+#import "ProfileControllerView.h"
+@interface ProfileControllerView()
+
+@end
+
+@implementation ProfileControllerView
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
